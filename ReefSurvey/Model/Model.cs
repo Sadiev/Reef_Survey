98e2868666b8f075c116e86c5abf3abb4dbd7f1f
Shod <dilshod.sadiev@outlook.com>
2019-08-22 08:38:18 -0400

    Model

---
+++ ReefSurvey/Model/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+
+
+    public class Fish
+    {
+        public int ID { get; set; }
+        public string ScientificName { get; set; }
+        public string FishLen { get; set; }
+
+    }
+
+
+}
