5601138cec1cd4e1f10f39aa33544109315e07ba
Shod <dilshod.sadiev@outlook.com>
2019-08-22 09:43:50 -0400

    Program

--- ReefSurvey/ReefSurvey/Program.cs
+++ ReefSurvey/ReefSurvey/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Model;
 
 namespace ReefSurvey
 {
@@ -6,7 +8,19 @@ namespace ReefSurvey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new FishDump())
+            {
+                //db.Blogs.Add(new Fish { Url = "http://blogs.msdn.com/adonet" });
+                //var count = db.SaveChanges();
+                //Console.WriteLine("{0} records saved to database", count);
+
+                //Console.WriteLine();
+                //Console.WriteLine("All blogs in database:");
+                //foreach (var blog in db.Blogs)
+                //{
+                //    Console.WriteLine(" - {0}", blog.Url);
+                //}
+            }
         }
     }
 }
